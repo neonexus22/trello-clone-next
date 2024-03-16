78211aa7c378a3d7a312914c56afa638cbb19649
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-03-16 21:49:52 +0545

    org setup

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,57 @@
+import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      {/* TODO: Mobile Sidebar */}
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+        <Button
+          size="sm"
+          className="rounded-sm hidden md:block h-auto py-1.5 px-2"
+        >
+          Create
+        </Button>
+        <Button className="rounded-sm block md:hidden">
+          <Plus className="size-4" />
+        </Button>
+      </div>
+      <div className="ml-auto flex items-center gap-x-2">
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl="/organization/:id"
+          afterSelectOrganizationUrl="/organization/:id"
+          afterLeaveOrganizationUrl="/select-org"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+            },
+          }}
+        />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: {
+                width: 30,
+                height: 30,
+              },
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
