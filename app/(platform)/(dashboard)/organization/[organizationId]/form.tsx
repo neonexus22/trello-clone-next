605db3e4fa1549ff3a489ad37a22b537fd1c5ec1
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-03-18 20:28:12 +0545

    create server component with validation

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,17 +1,30 @@
 "use client";
 
-import { FormState, create } from "@/actions/create-board";
 import { Button } from "@/components/ui/button";
 import { useFormState } from "react-dom";
 import { FormInput } from "./form-input";
 import { FormButton } from "./form-button";
+import { useAction } from "@/hooks/use-action";
+import { createBoard } from "@/actions/create-board";
 
 export const Form = () => {
-  const initialState: FormState = { message: null, errors: {} };
-  const [state, dispatch] = useFormState(create, initialState);
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess(data) {
+      console.log("success", { data });
+    },
+    onError(error) {
+      console.log({ error });
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    execute({ title });
+  };
+
   return (
-    <form action={dispatch}>
-      <FormInput errors={state?.errors} />
+    <form action={onSubmit}>
+      <FormInput errors={fieldErrors} />
       <FormButton />
     </form>
   );
