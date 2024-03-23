374567da80e2f70de5b26af2606fee576d3efe11
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-03-23 19:05:01 +0545

    create list

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -2,11 +2,19 @@
 
 import { ElementRef, useRef, useState } from "react";
 import { ListWrapper } from "./list-wrapper";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import { useEventListener, useOnClickOutside } from "usehooks-ts";
 import { FormInput } from "@/components/form/form-input";
+import { useParams, useRouter } from "next/navigation";
+import { FormSubmit } from "@/components/form/form-submit";
+import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/use-action";
+import { createList } from "@/actions/create-list";
+import { toast } from "sonner";
 
 export const ListForm = () => {
+  const params = useParams();
+  const router = useRouter();
   const formRef = useRef<ElementRef<"form">>(null);
   const inputRef = useRef<ElementRef<"input">>(null);
 
@@ -23,6 +31,17 @@ export const ListForm = () => {
     setIsEditing(false);
   };
 
+  const { execute, fieldErrors, isLoading } = useAction(createList, {
+    onSuccess(data) {
+      toast.success(`List "${data.title}" created!`);
+      disableEditing();
+      router.refresh();
+    },
+    onError(error) {
+      toast.error(error);
+    },
+  });
+
   const onKeyDown = (e: KeyboardEvent) => {
     if (e.key === "Escape") {
       disableEditing();
@@ -32,19 +51,39 @@ export const ListForm = () => {
   useEventListener("keydown", onKeyDown);
   useOnClickOutside(formRef, disableEditing);
 
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const boardId = formData.get("boardId") as string;
+    execute({ title, boardId });
+  };
+
   if (isEditing) {
     return (
       <ListWrapper>
         <form
+          action={onSubmit}
           ref={formRef}
           className="w-full p-3 rounded-md bg-white space-y-4 shadow-md"
         >
           <FormInput
             ref={inputRef}
+            errors={fieldErrors}
             id="title"
             className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
             placeholder="Enter list title..."
           />
+          <input hidden value={params.boardId} name="boardId" />
+          <div className="flex items-center gap-1">
+            <FormSubmit>Add List</FormSubmit>
+            <Button
+              disabled={isLoading}
+              onClick={disableEditing}
+              size="sm"
+              variant="ghost"
+            >
+              <X className="h-5 w-5" />
+            </Button>
+          </div>
         </form>
       </ListWrapper>
     );
