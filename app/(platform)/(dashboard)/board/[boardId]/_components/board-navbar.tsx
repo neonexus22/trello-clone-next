ea30476c1e2f7a6562741ec8e95f7527735bc2ac
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-03-23 15:23:43 +0545

    delete the board

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,7 +1,6 @@
-import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs";
 import { Board } from "@prisma/client";
 import { BoardTitleForm } from "./board-title-form";
+import { BoardOptions } from "./board-options";
 
 interface BoardNavbarProps {
   id: string;
@@ -11,6 +10,9 @@ export const BoardNavbar = async ({ id, data }: BoardNavbarProps) => {
   return (
     <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
       <BoardTitleForm data={data} />
+      <div className="ml-auto">
+        <BoardOptions id={data.id} />
+      </div>
     </div>
   );
 };
