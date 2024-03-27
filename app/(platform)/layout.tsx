57ef2815cb405b090d8f1ccec582b338f23674cd
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-03-27 12:10:56 +0545

    card update

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,3 +1,5 @@
+import ModalProvider from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ReactNode } from "react";
 import { Toaster } from "sonner";
@@ -5,8 +7,11 @@ import { Toaster } from "sonner";
 const PlatformLayout = ({ children }: { children: ReactNode }) => {
   return (
     <ClerkProvider>
-      <Toaster />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };
