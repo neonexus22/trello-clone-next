5fda0464e56ce7d7ca083d6a089946e0b61cd1f3
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-03-29 13:21:11 +0545

    card description submit

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -7,6 +7,7 @@ import { CardWithList } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { Header } from "./header";
+import { Description } from "./description";
 
 export const CardModal = () => {
   const cardModal = useCardModal();
@@ -20,6 +21,17 @@ export const CardModal = () => {
     <Dialog open={cardModal.isOpen} onOpenChange={cardModal.onClose}>
       <DialogContent>
         {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {!cardData ? (
+                <Description.Skeleton />
+              ) : (
+                <Description data={cardData} />
+              )}
+            </div>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
