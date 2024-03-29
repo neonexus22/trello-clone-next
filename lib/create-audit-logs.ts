6ecfc5bd08ec319eb1d6257d59afe5029ec0dc43
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-03-29 17:07:54 +0545

    added log action

---
+++ lib/create-audit-logs.ts
@@ -0,0 +1,39 @@
+import { auth, currentUser } from "@clerk/nextjs";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { db } from "./db";
+
+interface Props {
+  entityId: string;
+  entityType: ENTITY_TYPE;
+  entityTitle: string;
+  action: ACTION;
+}
+
+export const createAuditLog = async (props: Props) => {
+  const { orgId } = auth();
+  const user = await currentUser();
+
+  if (!orgId || !user) {
+    throw new Error("User not found!");
+  }
+
+  const { entityId, entityTitle, entityType, action } = props;
+
+  await db.auditLog.create({
+    data: {
+      orgId,
+      entityId,
+      entityTitle,
+      entityType,
+      action,
+      userId: user.id,
+      userImage: user?.imageUrl,
+      userName: user?.firstName + " " + user?.lastName,
+    },
+  });
+
+  try {
+  } catch (error) {
+    console.log("[AUDIT_LOG_ERROR]", error);
+  }
+};
